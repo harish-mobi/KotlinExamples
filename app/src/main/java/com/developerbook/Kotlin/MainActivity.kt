9f58359030bfb6ba942e6310df41f8de64a6f4b3
Harish <hari@Harishs-MacBook-Air.local>
2022-01-18 21:58:24 +0530

    recyclerview added

--- app/src/main/java/com/developerbook/Kotlin/MainActivity.kt
+++ app/src/main/java/com/developerbook/Kotlin/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.developerbook.Kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.developerbook.Kotlin.Recyclerview.RecyclerViewActivity
 
 class MainActivity : AppCompatActivity() {
+
+
+
+  private  lateinit var recyclerview:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerview=findViewById(R.id.recyclerview_btn)
+
+        recyclerview.setOnClickListener(View.OnClickListener { startActivity(Intent(this@MainActivity, RecyclerViewActivity::class.java))
+        })
     }
 }
